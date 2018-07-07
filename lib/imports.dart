267b85bc06bc5ad50a0d76116b06518b425815d2
Valdemar Grange <randomvald0069@gmail.com>
2018-07-07 22:47:07 +0200

    Fixed up some syntax library stuff

---
+++ lib/imports.dart
@@ -0,0 +1,5 @@
+library imports;
+
+import 'package:confirmationslider/confirmation_slider.dart';
+import 'package:confirmationslider/slider_decorations.dart';
+import 'package:confirmationslider/slider_icon.dart';
